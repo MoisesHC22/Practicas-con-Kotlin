fbe33299b82702d64c00c4c7f2352d3175a7abbb
MoisesHC <genesis2204gamerhc@gmail.com>
2024-06-03 00:22:38 -0600

    Incorporación de proyecto CambiarTipoDeDato

---
+++ CambiarTipoDeDatos/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+  // casteo o parseo
+  var sueldo = "2200.00"
+  
+    println("Transformando el dato: ${sueldo.toDouble()}")
+}

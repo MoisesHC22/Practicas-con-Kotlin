55de47866ab4c6bba0210b29963e74d7eedd478e
MoisesHC <genesis2204gamerhc@gmail.com>
2024-06-02 23:41:30 -0600

    Ejemplo de DeclaracionDeVariables

--- DeclaracionDeVariables/src/main/kotlin/Main.kt
+++ DeclaracionDeVariables/src/main/kotlin/Main.kt
@@ -7,12 +7,32 @@ fun main() {
     /*Kotlin utiliza valores Int, Double, String, Float*/
 
     // Constantes
-    val nombre = "Hola mundo"
-    val mensaje = "Con kotlin"
-    val edad = 21
 
-    //Concatenacion de las constantes
-    val resultado = nombre.plus(mensaje).plus(" ").plus(edad)
-    println("Iniciando con Kotlin: $resultado")
+    /* val nombre = "Hola mundo"
+       val mensaje = "Con kotlin"
+       val edad = 21
 
+       //Concatenación de las constantes
+       val resultado = nombre.plus(mensaje).plus(" ").plus(edad)
+       println("Iniciando con Kotlin: $resultado")
+    */
+
+    //Variable nunca se modifica
+    //Declaración larga
+    var variable: String = "Mi primer variable"
+    println(variable)
+
+    variable = "Cambie al segundo valor"
+    println(variable)
+
+    //Declaración corta
+    var nombre = "Juan Perez"
+    var edad = 21
+    var edadAdulto = 70
+
+    //Concatenación
+    println(nombre.plus(" Edad: ").plus(edad))
+
+    nombre = "Miguelito"
+    println(nombre.plus( " Edad: ").plus(edadAdulto))
 }

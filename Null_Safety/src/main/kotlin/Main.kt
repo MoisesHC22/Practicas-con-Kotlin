f0180f853dda57792eefa4a5b7d368c245b74d68
MoisesHC <genesis2204gamerhc@gmail.com>
2024-06-03 00:14:53 -0600

    Incorporación de proyecto de Null Safety

---
+++ Null_Safety/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    var edad: Int? = null
+
+   // edad = 12
+
+    edad?.let {
+        println("Este valor no viene vacio")
+    }
+
+}
